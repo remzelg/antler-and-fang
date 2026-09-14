f7367623af8c10cdb261d92963abe09588ae68b5
remzelg <remycarr@gmail.com>
2026-09-14 14:47:08 -0500

    whateva

--- scenes/board/board.gd
+++ scenes/board/board.gd
@@ -1,7 +1,7 @@
 class_name Board extends Node2D
 
 # This class has 2 main purposes
-# Display map and animations on said map
+# Display map
 # Store location data for all characters on map
 #  and make calculations against said data
 
@@ -30,29 +30,20 @@ func initialize(rect: Rect2i):
 			# note we could also populate character locations here if we wanted
 			board_state[point_id] = null
 
+# tile to world coords
+func map_to_local(tile):
+	return map.map_to_local(tile)
+
 func place(character_id, tile):
 	board_state[tile] = character_id
 	grid.fill(tile)
 
-func moving(character_id, path):
-	var coords = _tile_path_to_coords(path)
-	
-	temp_board_state[path[0]] = ""
-	temp_board_state[path[-1]] = character_id
-	temp_filled.push_front(path[0])
-	grid.fill(path[-1]) # actually mark destination tile solid
-
-func get_location_of(character_id):
-	var found_key
-	for key in board_state:
-		if board_state[key] == character_id:
-			found_key = key
-			break # Stop loop early
-	return found_key
-
-func get_path_from(from_tile, to_tile, to_closest_adjacent = false):
-	var path: Array[Vector2i] = grid.get_tile_path_from(from_tile, to_tile, to_closest_adjacent)
-	return path
+# fill the target tile, queue up state changes for after loop completion
+func moving(character_id, tile_path):
+	temp_board_state[tile_path[0]] = ""
+	temp_board_state[tile_path[-1]] = character_id
+	temp_filled.push_front(tile_path[0])
+	grid.fill(tile_path[-1]) # actually mark destination tile solid
 
 func cleanup():
 	for tile in temp_filled:
@@ -68,9 +59,71 @@ func cleanup():
 	temp_filled = []
 	temp_board_state = {}
 
-func map_to_local(tile):
-	return map.map_to_local(tile)
+func get_location_of(character_id):
+	var found_key
+	for key in board_state:
+		if board_state[key] == character_id:
+			found_key = key
+			break # Stop loop early
+	return found_key
 
+func get_path_from(from_tile, to_tile, to_closest_adjacent = false):
+	var path: Array[Vector2i] = grid.get_tile_path_from(from_tile, to_tile, to_closest_adjacent)
+	return path
+
+func null_or_adjacent_enemy_tiles(current_tile: Vector2i) -> Array[Vector2i]:
+	var directions = [
+		Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1),
+		Vector2i(-1,  0),                  Vector2i(1,  0),
+		Vector2i(-1,  1), Vector2i(0,  1), Vector2i(1,  1),
+	]
+	
+	var adjacent_enemies: Array[Vector2i] = []
+	var current_character_id = board_state[current_tile]
+	
+	for dir in directions:
+		var adj_tile = current_tile + dir
+		if board_state.has(adj_tile) and board_state[adj_tile] != null:
+			var character_id = board_state[adj_tile]
+			if !character_id.begins_with(current_character_id[0]):
+				adjacent_enemies.push_back(adj_tile)
+	
+	if adjacent_enemies != []:
+		return adjacent_enemies
+	
+	return []
+
+# TODO: Implmement ability to ignore filled tiles to find nearest enemy
+func null_or_nearest_enemy_tile(current_tile, ignore_filled_tiles=false) -> Vector2i:
+	var closest_tile = null
+	var min_path_length: int = 99
+	var ally_prefix = board_state[current_tile][0]
+	
+	for tile_pos in board_state:
+		var character_id = board_state[tile_pos]
+		
+		if !character_id:
+			continue
+		# Skip characters on your own team
+		if character_id.begins_with(ally_prefix):
+			continue
+		
+		# Get the path array of Vector2i points from your AStar grid
+		var path: Array[Vector2i] = get_path_from(current_tile, tile_pos, true)
+		
+		# If the path is empty, the enemy is completely trapped/unreachable
+		if path.is_empty():
+			continue
+			
+		# The length of the array corresponds to the actual walking distance
+		var path_length = path.size()
+		if path_length < min_path_length:
+			min_path_length = path_length
+			closest_tile = tile_pos
+		
+	return closest_tile
+
+#region Utils
 func _tile_path_to_coords(tiles):
 	var coords = []
 	for tile in tiles:
@@ -88,3 +141,4 @@ func _get_rect_from_vector2i_array(array: Array[Vector2i]) -> Rect2i:
 		rect = rect.expand(point)
 		
 	return rect
+#endregion
--- scenes/game.gd
+++ scenes/game.gd
@@ -78,16 +78,13 @@ func run_loop():
 	_cleanup()
 	previous_loop_complete = true
 
-func _is_dead(character):
-	return character.get_node("HealthBar").value <= 0
-
 func determine_action(character_id, current_tile):
-	var occupied_adj_tiles = _null_or_adjacent_enemy_tiles(current_tile)
+	var occupied_adj_tiles = board.null_or_adjacent_enemy_tiles(current_tile)
 	
 	if occupied_adj_tiles:
 		return ["attack", occupied_adj_tiles[0]]
 	
-	var nearest_enemy_tile = _null_or_nearest_enemy_tile(current_tile)
+	var nearest_enemy_tile = board.null_or_nearest_enemy_tile(current_tile)
 	if nearest_enemy_tile:
 		var path = board.get_path_from(current_tile, nearest_enemy_tile, true)
 		return ["move", path.slice(0,2)]
@@ -113,7 +110,6 @@ func conduct_action(character_id, current_tile, action, action_arg):
 	else: # if can't do anything
 		pass
 
-#region Display
 func place(character_id, tile):
 	var coords = board.map_to_local(tile)
 	var character = _find_character(character_id)
@@ -139,7 +135,7 @@ func move(character, coords, tiles):
 	var tween = create_tween()
 	for i in range(1, coords.size()): # skip path[0], the tile the character currently occupies
 		var destination_coords = Vector2(coords[i])
-		var facing = _determine_facing(tiles[i-1], tiles[i])
+		var facing = Utils.isometric.calculate_direction(tiles[i-1], tiles[i])
 		tween.tween_callback(character.play_animation.bind("walk", facing))
 		tween.tween_property(character, "position", destination_coords, walk_duration)
 	#tween.tween_callback(character.play_animation.bind("idle"))
@@ -166,23 +162,6 @@ func idle(character):
 		print(character.character_id + " idled")
 	character.play_animation("idle")
 
-# determine facing for animation purposes
-var VECTOR_TO_DIRECTION = {
-	Vector2i(0,-1): "front",
-	Vector2i(1,0): "right",
-	Vector2i(0,1): "back",
-	Vector2i(-1,0): "left",
-}
-
-func _determine_facing(origin, destination):
-	var vector = destination - origin
-	if abs(vector.x) > abs(vector.y):
-		return VECTOR_TO_DIRECTION[Vector2i(vector.x / abs(vector.x), 0)]
-	else:
-		return VECTOR_TO_DIRECTION[Vector2i(0, vector.y / abs(vector.y))]
-
-#endregion
-
 #region Private
 func _find_character(character_id):
 	var characters = get_tree().get_nodes_in_group("autobattler")
@@ -193,56 +172,8 @@ func _find_character(character_id):
 	
 	return null
 
-func _null_or_adjacent_enemy_tiles(current_tile: Vector2i):
-	var directions = [
-		Vector2i(-1, -1), Vector2i(0, -1), Vector2i(1, -1),
-		Vector2i(-1,  0),                  Vector2i(1,  0),
-		Vector2i(-1,  1), Vector2i(0,  1), Vector2i(1,  1),
-	]
-	
-	var adjacent_enemies = []
-	var current_character_id = board.board_state[current_tile]
-	
-	for dir in directions:
-		var adj_tile = current_tile + dir
-		if board.board_state.has(adj_tile) and board.board_state[adj_tile] != null:
-			var character_id = board.board_state[adj_tile]
-			if !character_id.begins_with(current_character_id[0]):
-				adjacent_enemies.push_back(adj_tile)
-	
-	if adjacent_enemies != []:
-		return adjacent_enemies
-	
-	return null
-
-func _null_or_nearest_enemy_tile(current_tile):
-	var closest_tile = null
-	var min_path_length: int = 99
-	var ally_prefix = board.board_state[current_tile][0]
-	
-	for tile_pos in board.board_state:
-		var character_id = board.board_state[tile_pos]
-		
-		if !character_id:
-			continue
-		# Skip characters on your own team
-		if character_id.begins_with(ally_prefix):
-			continue
-		
-		# Get the path array of Vector2i points from your AStar grid
-		var path: Array[Vector2i] = board.get_path_from(current_tile, tile_pos, true)
-		
-		# If the path is empty, the enemy is completely trapped/unreachable
-		if path.is_empty():
-			continue
-			
-		# The length of the array corresponds to the actual walking distance
-		var path_length = path.size()
-		if path_length < min_path_length:
-			min_path_length = path_length
-			closest_tile = tile_pos
-		
-	return closest_tile
+func _is_dead(character):
+	return character.get_node("HealthBar").value <= 0
 
 func _cleanup():
 	board.cleanup() # clear solid tiles after moving
